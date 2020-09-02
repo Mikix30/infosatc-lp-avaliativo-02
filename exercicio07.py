3570c1e076321b11aa88d3092500671e083d92c1
Mikix30 <juliabrunelli@outlook.com>
2020-09-02 18:07:08 -0300

    feat: Adiciona Exercício 07

---
+++ exercicio07.py
@@ -0,0 +1,8 @@
+#Loja online 
+carrinhodecompras = ["GeForce RTX 2080Ti", "cooler master", "placa mãe ASUS", "i9 9 geração"]
+print("seu carinho de compras tem: ", carrinhodecompras)
+#alguns itens apenas para um upgrade
+carrinhodecompras.remove("GeForce RTX 2080Ti")
+print("seu carrinho de compras agora tem: ",carrinhodecompras)
+carrinhodecompras.remove("cooler master")
+print("seu carrinho de compras agora tem: ",carrinhodecompras)

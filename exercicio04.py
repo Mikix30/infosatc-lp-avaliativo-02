68f0df4b07321acf9eb1d34e2405c18dcdf9d07a
Mikix30 <juliabrunelli@outlook.com>
2020-09-02 17:33:28 -0300

    feat: Adiciona Exercício 04

---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+#RECEITA DE VITAMITA:
+Lista1 = ["Banana", "Leite", "liquidificador"]
+print(Lista1)
+#DOBRANDO A RECEITA KK
+lista2 = Lista1.copy()
+print("OS INGREDIENTES DA VITAMINA SÃO: ", lista2)
+

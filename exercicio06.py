e539a1484d63cc64d05168c5816753d5e228190f
Mikix30 <juliabrunelli@outlook.com>
2020-09-02 17:56:58 -0300

    feat: Adiciona Exercício 06

---
+++ exercicio06.py
@@ -0,0 +1,14 @@
+
+listalivro = ["The Walking Dead", " Ready Player One"]
+print("Minha lista de livros tem: ",listalivro)
+listaesports = ["League of legends", "VALORANT","Paladins"]
+print("Minha lista de E-sports é composta por: ",listaesports)
+listafilme = ["Need for speed", "O espaço entre nós", "I'm mother", "TAU"]
+print("Minha lista de filmes para indicar tem os seguintes filmes: ", listafilme)
+listajogo = ["Osu!","Dead by daylight","Maplestory2","Dragon raja", "Don't starve", "subnautica", "subnautica below zero", "TFT"]
+print("minha pool de jogos é composta por: ", listajogo)
+
+listatudojunto = listaesports + listafilme + listajogo +listalivro
+print("A lista de todos os itens juntos é composta por: ",listatudojunto)
+
+print("Um dos livros da minha lista é: ",listalivro[1])

f95be28cd1ea149b75e1273506631e72c50949da
Mikix30 <juliabrunelli@outlook.com>
2020-09-02 17:38:30 -0300

    feat: Adiciona Exercício 05

---
+++ exercicio05.py
@@ -0,0 +1,8 @@
+#supermercado 
+minhalistadecompras = ["Leite","banana","maçã","morango"]
+print("Minha lista de compras tem: ", minhalistadecompras)
+
+if "morango" in minhalistadecompras:
+    print("Sim, você precisa comprar ele.")
+else:
+    print("Não, não vai na receita ou não precisa em casa.")
